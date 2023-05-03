16417a317fe23a426b841cce7a8e8fd602bcf32f
karthikganduri <karthikganduri@yahoo.in>
2023-05-03 16:02:19 +0530

    Render the titles in 2 columns

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import streamlit as st
-
+import pandas
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
     st.image("images\photo.jpeg", width=300)
-
 
 with col2:
     st.title("Karthik Ganduri")
@@ -19,3 +18,18 @@ content2 = """
 Below you can find some of the apps I have built in Python. Feel free to contact me !..........
 """
 st.write(content2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+col3, col4 = st.columns(2)
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
+
+
+
